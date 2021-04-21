69d186d27e82ac75d430c8d980fed9843fe9cb27
AndreDitVir01 <novaandre.saputraadit@gmail.com>
2021-04-22 06:58:50 +0700

    ONG: create view step 2, 3 for creating question grid; FIX: bug double routeing

--- routes/web.php
+++ routes/web.php
@@ -45,15 +45,4 @@ Route::group(['prefix' => 'user', 'middleware' => ['user']], function(){
     Route::get('step-1', [QuestionGridController::class, 'get_step_1'])->name('question_grid_step_1');
     Route::get('step-2', [QuestionGridController::class, 'get_step_2'])->name('question_grid_step_2');
     Route::get('step-3', [QuestionGridController::class, 'get_step_3'])->name('question_grid_step_3');;
-
-    Route::resource('basic-competency', BasicCompetencyController::class);
-    Route::resource('department', DepartmentController::class);
-    Route::resource('grade', GradeController::class);
-    Route::resource('lesson', LessonController::class);
-    Route::resource('profile', ProfileController::class);
-    Route::resource('question-card', QuestionCardController::class);
-    Route::resource('question-grid', QuestionGridController::class);
-    Route::resource('study', StudyController::class);
-    Route::resource('teacher', TeacherController::class);
-    Route::resource('user', UserController::class);
 });
